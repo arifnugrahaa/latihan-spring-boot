50ae05cc39bb786d6c3202c27812e592abbac0a7
arifnugrahaa <arifnugraha1243@gmail.com>
2022-07-23 21:00:48 +0700

    [handling form submission]

--- src/main/java/com/arif/controllers/HomeController.java
+++ src/main/java/com/arif/controllers/HomeController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.arif.entity.Product;
 import com.arif.service.ProductService;
 
 @Controller
@@ -21,5 +23,17 @@ public class HomeController {
         model.addAttribute("msg", message);
         model.addAttribute("products", productService.findAll());
         return "index";
+    }
+
+    @GetMapping("/add")
+    public String add(Model model){
+        model.addAttribute("product", new Product());
+        return "add";
+    }
+
+    @PostMapping("/save")
+    public String save(Product product, Model model){
+        productService.addProduct(product);
+        return "redirect:/";
     }
 }
--- src/main/java/com/arif/service/ProductService.java
+++ src/main/java/com/arif/service/ProductService.java
@@ -1,6 +1,6 @@
 package com.arif.service;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -10,15 +10,23 @@ import com.arif.entity.Product;
 @Service
 public class ProductService {
     
-    public static List<Product> products = Arrays.asList(
-        new Product(1L, "001", "Product 001", 1000.0),
-        new Product(2L, "002", "Product 002", 2000.0),
-        new Product(3L, "003", "Product 003", 3000.0),
-        new Product(4L, "004", "Product 004", 4000.0),
-        new Product(5L, "005", "Product 005", 5000.0)
-    );
+    public static List<Product> products = new ArrayList<Product>(){
+
+        {
+            add(new Product(System.currentTimeMillis(), "001", "Product 001", 1000.0));
+            add(new Product(System.currentTimeMillis(), "002", "Product 002", 2000.0));
+            add(new Product(System.currentTimeMillis(), "003", "Product 003", 3000.0));
+            add(new Product(System.currentTimeMillis(), "004", "Product 004", 4000.0));
+            add(new Product(System.currentTimeMillis(), "005", "Product 005", 5000.0));
+        }
+    };
 
     public List<Product> findAll(){
         return products;
+    }
+
+    public void addProduct(Product product){
+        product.setId(System.currentTimeMillis());
+        products.add(product);
     }
 }
